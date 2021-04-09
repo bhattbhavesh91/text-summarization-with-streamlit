d2423ee6ab44202b731f7b4aa503f7ccba652548
Bhavesh Bhatt <20332240+bhattbhavesh91@users.noreply.github.com>
2021-04-09 12:35:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,33 @@ from transformers import pipeline
 
 @st.cache(allow_output_mutation=True)
 def load_summarizer():
-    model = pipeline("summarization")
+    model = pipeline("summarization", device=0)
     return model
+
+
+def generate_chunks(inp_str):
+    max_chunk = 500
+    inp_str = inp_str.replace('.', '.<eos>')
+    inp_str = inp_str.replace('?', '?<eos>')
+    inp_str = inp_str.replace('!', '!<eos>')
+    
+    sentences = inp_str.split('<eos>')
+    current_chunk = 0 
+    chunks = []
+    for sentence in sentences:
+        if len(chunks) == current_chunk + 1: 
+            if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk:
+                chunks[current_chunk].extend(sentence.split(' '))
+            else:
+                current_chunk += 1
+                chunks.append(sentence.split(' '))
+        else:
+            chunks.append(sentence.split(' '))
+
+    for chunk_id in range(len(chunks)):
+        chunks[chunk_id] = ' '.join(chunks[chunk_id])
+    return chunks
+
 
 summarizer = load_summarizer()
 st.title("Summarize Text")
@@ -16,5 +41,11 @@ min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
 do_sample = st.sidebar.checkbox("Do sample", value=False)
 with st.spinner("Generating Summary.."):
     if button and sentence:
-        result = summarizer(sentence, max_length=max, min_length=min, do_sample=do_sample)
-        st.write(result[0]['summary_text'])
+        chunks = generate_chunks(sentence)
+        res = summarizer(chunks,
+                         max_length=max, 
+                         min_length=min, 
+                         do_sample=do_sample)
+        text = ' '.join([summ['summary_text'] for summ in res])
+        # st.write(result[0]['summary_text'])
+        st.write(text)
